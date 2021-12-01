77cc7b7e5469897485d28be51ca528c5fa9647ca
kennjr <ken.juniour@student.moringaschool.com>
2021-12-01 22:01:35 +0300

    Added the fun for getting the visibility of a category's content based on the id of the clicked arrow

--- js/courses_script.js
+++ js/courses_script.js
@@ -2,3 +2,21 @@ let codingCourses_visible = true;
 let microsoftCourses_visible = true;
 let networkingCourses_visible = true;
 let introCourses_visible = true;
+
+
+
+// This fun. will get us the visibility of course list, for the clicked arrow
+function getClickedArrow (id) {
+    switch (id) {
+        case "microsoft_courses_list":
+            return microsoftCourses_visible;
+        case "intro_courses_list":
+            return introCourses_visible;
+        case "networking_courses_list":
+            return networkingCourses_visible;
+        case "coding_courses_list":
+            return codingCourses_visible;
+        default:
+            break;
+    }
+}
